a6b5b2d288be5bcef67051c2e89d5c66ee8e2587
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-01-13 17:30:35 +0530

    post

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ const userModel = require("./models/user");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("hello word");
 });
 app.get("/form", (req, res) => {
   res.render("index");
 });
-app.get("/form-data" ,(req ,res)=>{
-  console.log(req.query);
+// app.get("/form-data" ,(req ,res)=>{
+//   console.log(req.query);
+//   res.send("form dan");
+// });
+app.post("/form-data", (req, res) => {
+  console.log(req.body);
   res.send("form dan");
 });
 app.listen(30001);
