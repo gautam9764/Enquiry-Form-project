140da6c451ed7265934dff7398c84baf23d599e8
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-01-13 16:16:14 +0530

    form style 1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const confidb = require("./config/db");
 const userModel = require("./models/user");
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("hello word");
@@ -13,4 +14,4 @@ app.get("/form", (req, res) => {
   res.render("index");
 });
 
-app.listen(3000);
+app.listen(30001);
